7c285f074c66312e539bd382efcfa474db7bb92f
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-10-02 19:11:02 -0700

    functioning game pre-save/load functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,26 @@
+require 'yaml'
+
+class GameTools
+
+    def initialize(word)
+        @word = word
+    end
+
+    def word_array(word)
+        @word_arr = @word.split('')
+        @word_arr.pop()
+        @word_arr
+    end
+
+    def correct_letters_array(word)
+        @correct_letters_array = Array.new(word.length, '_')
+        @correct_letters_array.pop()
+        @correct_letters_array
+    end
+
+end
+
+
 def random_word()
     word = ""
     while word.length < 6 || word.length > 12
@@ -6,34 +29,54 @@ def random_word()
     word
 end
 
-def word_array(word)
-    word_arr = word.split('')
-    word_arr.pop()
-    word_arr
-end
-
-
 secret_word = random_word()
-word_arr = word_array(secret_word)
-correct_letters_array = Array.new(secret_word.length, '_')
+holder = secret_word.split('')
+holder.pop()
+secret_word = holder.join()
 
-puts "#{correct_letters_array.join()}\n\n
-Welcome to Hangman! The computer has randomly selected a word for you (between 5 and 12 letters). Guess a letter. You have X guesses."
-guess = gets.chomp.downcase
-i = 0
-word_arr.each_with_index do | letter, index |
-    index < secret_word.length
-    if letter == guess
-        correct_letters_array[index] = guess
-        i += 1
-    else
-        i += 0
+hangman = GameTools.new(secret_word)
+
+puts "Welcome to Hangman! The computer has randomly selected a word for you (between 5 and 12 letters). Guess a letter. You have 6 guesses.\n\n
+Would you like to reload an old save?"
+load = gets.chomp.downcase
+if load == 'yes'
+    YAML.load(File.read('saves/yaml.dump'))
+elsif load == 'no'
+    word_array = hangman.word_array(secret_word)
+    correct_letters_array = hangman.correct_letters_array(secret_word)
+else puts "Sorry, please type 'yes' or 'no'"
+end
+
+tries = 0
+win = false
+puts secret_word
+    
+
+    while tries < 6
+        puts "\n\n#{correct_letters_array.join()}\n\n
+        You have #{7 - tries - 1} guesses left! Which letter would you like to guess?
+        You can also type 'save' to save your game and come back later!"
+        guess = gets.chomp.downcase
+        i = 0
+        word_array.each_with_index do | letter, index |
+            index < secret_word.length
+            if letter == guess
+                correct_letters_array[index] = guess
+                i += 1
+            else
+                i += 0
+            end
+        end
+        if i == 0
+            puts "Sorry, that letter isn't in this word."
+            tries += 1
+        elsif correct_letters_array.join() == word_array.join
+            tries = 7
+            win = true
+        end
     end
-end
-
-if i == 0
-    puts "Sorry, that letter isn't in this word."
-end
-
-puts correct_letters_array.join()
-
+    if win == true
+        puts "You got it! The word was #{secret_word}."
+    else
+        puts "Better luck next time, champ. The word was #{secret_word}."
+    end
